2be043627d7594891f9e2e3649eb033403d87ea2
Rajvardhan182002 <124658564+Rajvardhan182002@users.noreply.github.com>
2023-09-13 14:49:15 +0530

    Create img.py

---
+++ img.py
@@ -0,0 +1,7 @@
+from PIL import Image
+  
+# open method used to open different extension image file
+im = Image.open(r"C:\Users\E & TC\Downloads\lion.jpg") 
+  
+# This method will show image in any image viewer 
+im.show() 
